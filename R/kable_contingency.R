9446c40766e010d837a89ca9794767b329b11ea5
annahyan <ahakobyan@cemm.oeaw.ac.at>
2022-05-16 17:29:39 +0200

    more universal answer

--- R/kable_contingency.R
+++ R/kable_contingency.R
@@ -6,12 +6,14 @@
 kable_contingency = function(x) {
 col1 = colnames(x)[1]
 col2 = colnames(x)[2]
-    x %>% group_by_all() %>% 
+cont.table = x %>% group_by_all() %>% 
         tally () %>% 
-        spread(key = 1, value = n) %>% 
-        rename("\t" = all_of(col2)) %>% add_column(" " = c(col2, " "), .before = "\t") %>% 
+        spread(key = 1, value = n) 
+cont.table  %>% 
+        rename("\t" = all_of(col2)) %>% 
+        add_column(" " = c(col2, rep(" ", nrow(.) - 1)), .before = "\t") %>% 
         knitr::kable() %>% 
         kable_minimal(position = "left",full_width = F) %>% 
-        add_header_above(c(" ", " ", col1, ""), escape = FALSE) %>% 
+        add_header_above(c(" ", " ", col1, rep(" ", ncol(cont.table) - 2))) %>% 
         column_spec(1:2, bold = TRUE)
 }
